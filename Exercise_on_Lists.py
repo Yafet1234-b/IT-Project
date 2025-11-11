150a0417807eb79bbeec48bbaef1a4739144e494
Yafet1234-b <yafet2028@hmacademy.org>
2025-11-11 11:40:12 +0300

    Update Exercise_on_Lists.py

--- Exercise_on_Lists.py
+++ Exercise_on_Lists.py
@@ -1,13 +1,13 @@
-Inputted_Name = input(" Enter the name of the contact you want to add : ")
-Inputted_Contact_Number = input(" Enter the phone number of the contact you want to add : ")
-Removed_Contact_Name = input(" Enter the name of the contact you want to remove : ")
+Name_of_Contact = input(" Enter the name of the contact you want to add : ")
+Phone_Number_of_Contact = input(" Enter the phone number of the contact you want to add : ")
+Removed = input(" Enter the name of the contact you want to remove : ")
 Contacts = []
 def Contact(x, y, z):
     Contacts.append(x)
     Contacts.append(y)
     if z in Contacts:
-        Contacts.remove(Removed_Contact_Name)
+        Contacts.remove(Removed)
     else:
         print(f"{z} does not exist in your contacts.")
     return(Contacts)
-print(Contact(Inputted_Name, Inputted_Contact_Number, Removed_Contact_Name))
+print(Contact(Name_of_Contact, Phone_Number_of_Contact, Removed))
